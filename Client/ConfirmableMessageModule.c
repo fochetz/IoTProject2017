35e30152a65b3704259c5f698ed0c310b6229aba
Luca Fochetta <fochetz@msn.com>
2017-07-08 18:25:17 +0200

    file removed

--- Client/ConfirmableMessageModule.c
+++
@@ -1,7 +0,0 @@
-interface ConfirmableMessageModule {
-	
-	event void confirmationReceived(uint_8 id);
-	//event void connectionRequestReceived(uint_8 id);
-	command void sendConfirmableMessage(uint_8 destination, payload);
-	
-}
